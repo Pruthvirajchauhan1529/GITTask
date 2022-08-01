51a3880b90d034fce8c52c4152b591c221f0b759
Pruthviraj Chauhan <pruthviraj@7span.com>
2022-08-01 16:17:07 +0530

    feature03 first changes commit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    public void m1(){
+        System.out.println("This test m1 method..");
+    }
+    public static void m2() {
+        System.out.println("This test static m2 method..");
+    }
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.m1();
+        m2();
+    }
+}
