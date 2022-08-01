d4a06c4a55f6dd16cf2433dbb428b5d858b04c90
Pruthviraj Chauhan <pruthviraj@7span.com>
2022-08-01 15:08:09 +0530

    test02 feature02

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("featur02");
+        System.out.println("feature02");
     }
 }
---
+++ src/Sub.java
@@ -0,0 +1,10 @@
+public class Sub {
+    public static void main(String[] args) {
+
+        for (int i = 1; i<=10;i++){
+            int c;
+            c = i * 2;
+            System.out.println(i +" * 2 = "+c);
+        }
+    }
+}
