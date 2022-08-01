08fd6733c194c6e7c6f375e358b654789d25f607
Pruthviraj Chauhan <pruthviraj@7span.com>
2022-08-01 16:28:31 +0530

    feature03 test02 changes commit

--- src/Test.java
+++ src/Test.java
@@ -9,6 +9,7 @@ public class Test {
     public static void main(String[] args) {
         Test test = new Test();
         test.m1();
+        System.out.println("Hello Akshaybhai...");
         m2();
     }
 }
